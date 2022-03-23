abe74f691bfe5a0baded8fb1baacfcd5c592ad42
nutthanon <nutthanon.tho@gmail.com>
2022-03-24 01:16:25 +0700

    [+] MergeSort.go

--- Algorithms/Sort/MergeSort/MergeSort.go
+++ Algorithms/Sort/MergeSort/MergeSort.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import "fmt"
 
+func merge(a []int, b []int) []int {
+	final := []int{}
+	i := 0
+	j := 0
+	for i < len(a) && j < len(b) {
+		if a[i] < b[j] {
+			final = append(final, a[i])
+			i++
+		} else {
+			final = append(final, b[j])
+			j++
+		}
+	}
+	for ; i < len(a); i++ {
+		final = append(final, a[i])
+	}
+	for ; j < len(b); j++ {
+		final = append(final, b[j])
+	}
+	return final
+}
+
+func mergeSort(items []int) []int {
+	if len(items) < 2 {
+		return items
+	}
+	first := mergeSort(items[:len(items)/2])
+	second := mergeSort(items[len(items)/2:])
+	return merge(first, second)
+}
+
+func main() {
+	unsorted := []int{10, 6, 2, 1, 5, 8, 3, 4, 7, 9}
+	sorted := mergeSort(unsorted)
+	fmt.Println(sorted)
 }
