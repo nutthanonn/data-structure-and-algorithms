a80ab1a916e1b2bec7996c50853a9e44c7fc9f92
nutthanon <nutthanon.tho@gmail.com>
2022-04-11 19:50:22 +0700

    add LevelOrderTree.go

---
+++ DataStructure/Tree/LeverOrderTree/LevelOrderTree.go
@@ -0,0 +1,59 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+}
+
+func (this *Node) Insert(val int) {
+	new_node := &Node{}
+	new_node.Val = val
+
+	if val > this.Val {
+		if this.Right == nil {
+			this.Right = new_node
+		} else {
+			this.Right.Insert(val)
+		}
+	} else if val < this.Val {
+		if this.Left == nil {
+			this.Left = new_node
+		} else {
+			this.Left.Insert(val)
+		}
+	}
+}
+func (this *Node) LevelOrder() {
+	if this == nil {
+		return
+	}
+
+	q := []*Node{}
+	q = append(q, this) //push
+
+	for len(q) != 0 {
+		ptr := q[0]
+		fmt.Println(ptr.Val)
+		q = q[1:] //pop
+
+		if ptr.Left != nil {
+			q = append(q, ptr.Left)
+		}
+
+		if ptr.Right != nil {
+			q = append(q, ptr.Right)
+		}
+	}
+
+}
+
+func main() {
+	t := &Node{Val: 100}
+	t.Insert(150)
+	t.Insert(50)
+
+	t.LevelOrder()
+}
