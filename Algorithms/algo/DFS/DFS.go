2e3fc51e3a6d51be6ebf89c70920b274677cac5b
nutthanon <nutthanon.tho@gmail.com>
2022-04-11 22:23:08 +0700

    update function-dfs-spanning-tree

--- Algorithms/algo/DFS/DFS.go
+++ Algorithms/algo/DFS/DFS.go
@@ -39,6 +39,41 @@ func (this *Node) DFS() {
 
 // ########################################################################################################################################################################
 
+//Spanning Tree
+
+type Graph struct {
+	graph map[string][]string
+}
+
+type Stack struct {
+	s []string
+}
+
+func newGraph() *Graph {
+	return &Graph{graph: make(map[string][]string)}
+}
+
+func (this *Graph) Insert(first, last string) {
+	this.graph[first] = append(this.graph[first], last)
+	this.graph[last] = append(this.graph[last], first)
+}
+
+func (this *Stack) DFS_Spanning_Tree(graph map[string][]string, node string) {
+	check := true
+	for _, v := range this.s {
+		if v == node {
+			check = false
+		}
+	}
+
+	if check {
+		this.s = append(this.s, node)
+		for _, v := range graph[node] {
+			this.DFS_Spanning_Tree(graph, v)
+		}
+	}
+}
+
 func main() {
 	t := &Node{Val: 100}
 	t.Insert(110)
@@ -49,6 +84,36 @@ func main() {
 	t.Insert(50)
 	t.Insert(20)
 	t.Insert(90)
-
 	// t.DFS()
+
+	// ########################################################################################################################################################################
+
+	/*
+
+		graph = {
+		 	'A' : ['B', 'C', 'D'],
+		 	'B' : ['A', 'C', 'D'],
+		 	'C' : ['A', 'B', 'E'],
+		 	'D' : ['A', 'B', 'E', 'F'],
+		 	'E' : ['C', 'D', 'F'],
+		 	'F' : ['D', 'E'],
+		}
+
+	*/
+
+	g := newGraph()
+	s := &Stack{}
+
+	g.Insert("A", "B")
+	g.Insert("A", "C")
+	g.Insert("A", "D")
+	g.Insert("B", "C")
+	g.Insert("B", "D")
+	g.Insert("D", "E")
+	g.Insert("D", "F")
+	g.Insert("E", "C")
+	g.Insert("E", "F")
+
+	s.DFS_Spanning_Tree(g.graph, "A")
+	fmt.Println(s)
 }
