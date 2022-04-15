e3f4772adfa539e6feab202033e16752a024d6da
nutthanon <nutthanon.tho@gmail.com>
2022-04-15 17:22:44 +0700

    add PostOrderTree.go

---
+++ DataStructure/Tree/PostOrderTree/PostOrderTree.go
@@ -0,0 +1,69 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	val   int
+	Left  *Node
+	Right *Node
+}
+
+type InorderArr struct {
+	arr  []int
+	size int
+}
+
+func (n *Node) Insert(val int) {
+	new_node := Node{}
+	new_node.val = val
+	if n.val > val {
+		if n.Left == nil {
+			n.Left = &new_node
+		} else {
+			n.Left.Insert(val)
+		}
+	} else if n.val < val {
+		if n.Right == nil {
+			n.Right = &new_node
+		} else {
+			n.Right.Insert(val)
+		}
+	}
+}
+
+func (n *Node) Search(val int) bool {
+	if n == nil {
+		return false
+	}
+
+	if n.val > val {
+		return n.Left.Search(val)
+	} else if n.val < val {
+		return n.Right.Search(val)
+	}
+
+	return true
+}
+
+func (i *InorderArr) PostOrder(n *Node) {
+	if n == nil {
+		return
+	}
+
+	i.PostOrder(n.Left)
+	i.PostOrder(n.Right)
+	i.size++
+	i.arr = append(i.arr, n.val)
+}
+
+func main() {
+	tree := &Node{val: 25}
+	inorder := &InorderArr{}
+	node := []int{15, 50, 10, 22, 35, 70, 4, 12, 18, 24, 31, 44, 66, 90}
+	for i := 0; i < len(node); i++ {
+		tree.Insert(node[i])
+	}
+	inorder.PostOrder(tree)
+	fmt.Println(inorder)
+
+}
