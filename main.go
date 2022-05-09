d128ff9b7addd103689c969e44d5f382752c71fa
nutthanon <nutthanon.tho@gmail.com>
2022-05-09 15:52:55 +0700

    :rice_ball:

--- main.go
+++ main.go
@@ -1,16 +1,10 @@
+/*
+Bellman Ford Algorithm Dynamic Programming
+
+*/
+
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-)
-
 func main() {
-	fileCount := map[string]int{
-		"cpp": 10,
-		"js":  8,
-		"go":  10,
-	}
-	bytes, _ := json.MarshalIndent(fileCount, "", "  ")
-	fmt.Println(string(bytes))
+
 }
