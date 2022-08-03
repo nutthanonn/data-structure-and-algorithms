397114d432a7fc6dd821c77f44c46a91cf49e185
nutthanon <nutthanon.tho@gmail.com>
2022-08-03 22:41:11 +0700

    [+] update: linkedlist.c

--- DataStructure/linkedlist/linkedlist.c
+++ DataStructure/linkedlist/linkedlist.c
@@ -2,54 +2,97 @@
 #include <stdlib.h>
 
 
-
-typedef struct Node {
-    int Val;
-    struct Node * Next;
+typedef struct node
+{
+    int val;
+    struct node *next;
 } node_t;
 
-node_t * head = NULL;
+
+int getSize(node_t *head)
+{
+    node_t *curr = head;
+    int size = 0;
+    while (curr != NULL)
+    {
+        size++;
+        curr = curr->next;
+    }
+    return size;
+}
 
 
-void Insert(node_t** head,int val) {
-    node_t * new_node = (node_t *)malloc(sizeof(node_t));
-    node_t * current = *head;
+void push(node_t *head, int val)
+{
+    node_t *curr = head;
+    while (curr->next != NULL)
+        curr = curr->next;
+
+    node_t *new_node = (node_t*)malloc(sizeof(node_t));
+    new_node->val = val;
+    new_node->next = NULL;
+    curr->next = new_node;
+}
 
 
-    new_node->Val  = val;
-    new_node->Next = NULL;
+void getLinkedlist(node_t *head)
+{
+    node_t *curr = head;
+    while (curr != NULL)
+    {
+        printf("%d\n", curr->val);
+        curr = curr->next;
+    }
+}
 
-    if(*head == NULL) {
-        *head = new_node;   
+
+void InsertIndex(node_t *head, int val, int pos)
+{
+    if(pos < 0 || pos > getSize(head))
         return;
-    }
 
-    while (current -> Next != NULL) {
-        current = current -> Next;
-    }
-    current->Next = new_node;
-    return;
+    node_t *curr = head;
+    for(int i=0;i<pos-1;i++)
+        curr = curr->next;
+
+    node_t *new_node = (node_t*)malloc(sizeof(node_t));
+    new_node->val = val;
+    new_node->next = curr->next;
+    curr->next = new_node;
+}
+
+void delIndex(node_t *head, int pos)
+{
+    node_t *curr = head;
+    for(int i=0;i<pos-1;i++)
+        curr = curr->next;
+
+    curr->next = curr->next->next;
 }
 
 
 
-void Data() {
-    struct Node *current = head;
-    while (current != NULL) {   
-        printf("%d\n", current->Val);
-        current = current->Next;
-    }
-}
+int main()
+{
+    int number[5] = {1,2,3,4,5};
+    node_t *head = NULL;
+    head = (node_t*)malloc(sizeof(node_t));
+    head->val = 0;
+
+    for(int i=0;i<5;i++)
+        push(head, number[i]);
 
 
+    getLinkedlist(head);
+    printf("------\n");
+    InsertIndex(head, 99, 7);
+    getLinkedlist(head);
+    printf("------\n");
 
-int main(int argc, char const *argv[]) {
-    Insert(&head, 1);
-    Insert(&head, 2);
-    Insert(&head, 3);
-    Insert(&head, 4);
-    Data(); 
-    
+
+    delIndex(head, 1);
+    getLinkedlist(head);
+
 
     return 0;
 }
